9258aa2e0122910d26bdae78cbd36e72b5774057
janithgamage1.ed <janithgamage1.ed@gmail.com>
2023-10-03 00:33:02 +0530

    feat: update desc : user implimention rest on all the business logic and DB configuration

---
+++ Rest/Entities/UserDetails.cs
@@ -0,0 +1,54 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace Rest.Entities
+{
+    public class UserDetails
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("NIC")]
+        public string NIC { get; set; } // National Identification Card
+
+        [BsonElement("Email")]
+        public string Email { get; set; }
+
+        [BsonElement("ContactNo")]
+        public string ContactNo { get; set; }
+
+        [BsonElement("Salutation")]
+        public string Salutation { get; set; }
+
+        [BsonElement("FirstName")]
+        public string FirstName { get; set; }
+
+        [BsonElement("LastName")]
+        public string LastName { get; set; }
+
+        [BsonElement("UserRole")]
+        public string UserRole { get; set; }
+
+        [BsonElement("IsActive")]
+        public bool IsActive { get; set; }
+
+        [BsonElement("Status")]
+        public string Status { get; set; }
+
+        [BsonElement("ActivationStatus")]
+        public string ActivationStatus { get; set; }
+
+        [BsonElement("CreatedOn")]
+        public DateTime CreatedOn { get; set; }
+
+        [BsonElement("UpdatedOn")]
+        public DateTime UpdatedOn { get; set; }
+
+        [BsonElement("CreatedBy")]
+        public string CreatedBy { get; set; }
+
+        [BsonElement("UpdatedBy")]
+        public string UpdatedBy { get; set; }
+    }
+}
--- Rest/Program.cs
+++ Rest/Program.cs
@@ -6,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.Configure<ProductDBSettings>(builder.Configuration.GetSection("ProductDatabase"));
 builder.Services.AddSingleton<IProductService, ProductService>();
+builder.Services.AddSingleton<IUserService, UserService>();
 builder.Services.AddControllers(); 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
