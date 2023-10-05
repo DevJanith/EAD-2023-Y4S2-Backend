5ffa030ac48c8de90a20c64289a53975747dc771
janithgamage1.ed <janithgamage1.ed@gmail.com>
2023-10-05 09:06:57 +0530

    fix ; update

--- Rest/Configurations/ProductDBSettings.cs
+++ Rest/Configurations/ProductDBSettings.cs
@@ -6,6 +6,9 @@
         public string DatabaseName { get; set; }
         public string ProductCollectionName { get; set; }
         public string UserCollectionName { get; set; }
+        public string ReservationCollectionName { get; set; }
+        public string ScheduleCollectionName { get; set;}
+        public string TrainCollectionName { get;set; }
     }
 
 }
